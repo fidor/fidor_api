f2e22e7f9aa2c02d40e809b393862635ff762d5e
yulian savov <yuliansavov@gmail.com>
2018-10-29 17:20:54 +0200

    Adds DSL for fetching transactions of an account (#11)

---
+++ lib/fidor_api/client/dsl/accounts.rb
@@ -0,0 +1,11 @@
+module FidorApi
+  class Client
+    module DSL
+      module Accounts
+        def account_transactions(id, options = {})
+          fetch(:collection, Model::Transaction, "/accounts/#{id}/transactions", options)
+        end
+      end
+    end
+  end
+end
---
+++ spec/features/dsl/accounts_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+RSpec.describe 'DSL - Accounts' do
+  let(:client)        { setup_client }
+  let(:client_id)     { 'client-id' }
+  let(:client_secret) { 'client-secret' }
+
+  describe '#account_transactions' do
+    before do
+      stub_fetch_request(endpoint: %r{/accounts/123456/transactions}, response_body: [{ id: 42 }, { id: 43 }])
+    end
+
+    it 'returns a collection of transactions' do
+      transactions = client.account_transactions('123456')
+      expect(transactions).to be_instance_of FidorApi::Collection
+
+      transaction = transactions.first
+      expect(transaction).to be_instance_of FidorApi::Model::Transaction
+      expect(transaction.id).to eq 42
+    end
+  end
+end
