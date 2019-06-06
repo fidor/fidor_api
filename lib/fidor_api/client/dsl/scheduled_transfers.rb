b8a9d2ac6f644723e787c3d57ef196db27bfa7a3
Amine4fidor <belkhiria@fidor.com>
2019-06-06 23:13:56 +0200

    sched. transfers + stand. order create w/ options

--- lib/fidor_api/client/dsl/scheduled_transfers.rb
+++ lib/fidor_api/client/dsl/scheduled_transfers.rb
@@ -3,7 +3,27 @@ module FidorApi
     module DSL
       module ScheduledTransfers
         def scheduled_transfers(options = {})
-          fetch(:collection, Model::ScheduledTransfer, 'scheduled_transfers', options)
+          fetch(:collection, FidorApi::Model::ScheduledTransfer, 'scheduled_transfers', options)
+        end
+
+        def scheduled_transfer(id, options = {})
+          fetch(:single, FidorApi::Model::ScheduledTransfer, "scheduled_transfers/#{id}", options)
+        end
+
+        def new_scheduled_transfer(attributes = {})
+          FidorApi::Model::ScheduledTransfer.new(attributes)
+        end
+
+        def create_scheduled_transfer(attributes = {}, options = {})
+          create(FidorApi::Model::ScheduledTransfer, 'scheduled_transfers', attributes, options)
+        end
+
+        def update_scheduled_transfer(id, attributes = {}, options = {})
+          update(FidorApi::Model::ScheduledTransfer, "scheduled_transfers/#{id}", id, attributes, options)
+        end
+
+        def confirm_scheduled_transfer(id, options = {})
+          request(:put, "scheduled_transfers/#{id}/confirm", {}, options.delete(:headers)).headers['Location']
         end
       end
     end
