c474a1f67a46df0f484c25e5e4b6248a77fcae45
Konstantin Munteanu <munteanu@fidor.com>
2019-07-02 14:09:12 +0200

    rubocop

--- spec/features/dsl/transfers/generic_spec.rb
+++ spec/features/dsl/transfers/generic_spec.rb
@@ -137,7 +137,7 @@ RSpec.describe 'DSL - Transfers - Generic' do
         endpoint:         %r{/transfers/92bf870d-d914-4757-8691-7f8092a77e0e/confirm},
         request_headers:  request_headers,
         response_body:    {
-          id: 'eb5e8e0d-4611-4124-a1c5-f0b1afad250b',
+          id:    'eb5e8e0d-4611-4124-a1c5-f0b1afad250b',
           links: { redirect: redirect_link }
         },
         response_headers: { 'Location' => location },
